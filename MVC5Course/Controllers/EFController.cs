a7c716a0154db41c65c83065c2feed5738bdfc0d
邱冠傑 <kuanchieh1102@hotmail.com>
2016-03-06 13:32:26 +0800

    Modify Delete process

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -13,39 +13,26 @@ namespace MVC5Course.Controllers
 
       public ActionResult Index() {
 
-         var product = new Product() {
-            ProductName = "BMV",
-            Price = 2,
-            Stock = 1,
-            Active = true
-         };
+         //var product = new Product() {
+         //   ProductName = "BMV",
+         //   Price = 2,
+         //   Stock = 1,
+         //   Active = true
+         //};
 
-         db.Products.Add(product);
+         //db.Products.Add(product);
 
-         try {
-
-            db.SaveChanges();
-
-         }
-         catch (DbEntityValidationException ex) {
-
-            foreach (DbEntityValidationResult item in ex.EntityValidationErrors) {
-               string entityName = item.Entry.GetType().Name;
-
-               foreach (DbValidationError error in item.ValidationErrors) {
-                  throw new Exception(entityName + " 類型驗證失敗: " + error.ErrorMessage);
-               }
-            }
-
-            throw;
-         }
-
-
-         var pkey = product.ProductId;
+         //var pkey = product.ProductId;
 
          //var data = db.Products.Where(p => p.ProductId == pkey).ToList();
 
-         var data = db.Products.OrderByDescending(p => p.ProductId);
+         var data = db.Products.OrderByDescending(p => p.ProductId).Take(10);
+
+         foreach (var item in data) {
+            item.Price = item.Price + 1;
+         }
+
+         SaveChanges();
 
          return View(data);
       }
@@ -55,6 +42,33 @@ namespace MVC5Course.Controllers
          var data = db.Products.FirstOrDefault(p => p.ProductId == Id);
 
          return View(data);
+      }
+
+      public ActionResult Delete(int id) {
+         var item = db.Products.Find(id);
+
+         db.OrderLines.RemoveRange(item.OrderLines);
+         db.Products.Remove(item);
+         db.SaveChanges();
+
+         return RedirectToAction("Index");
+      }
+
+      private void SaveChanges() {
+         try {
+
+            db.SaveChanges();
+
+         }
+         catch (DbEntityValidationException ex) {
+            foreach (DbEntityValidationResult item in ex.EntityValidationErrors) {
+               string entityName = item.Entry.GetType().Name;
+
+               foreach (DbValidationError error in item.ValidationErrors) {
+                  throw new Exception(entityName + " 類型驗證失敗: " + error.ErrorMessage);
+               }
+            }
+         }
       }
    }
 }
