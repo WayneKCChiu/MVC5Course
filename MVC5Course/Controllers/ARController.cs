301548c0a9307eb71b27bf1ad793d320ed9f07e8
邱冠傑 <kuanchieh1102@hotmail.com>
2016-03-12 13:54:12 +0800

    ActionResult 用法 ( View, PartialView, Content, File )

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -1,13 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using System.Text;
+using System.Web.Mvc;
 
 namespace MVC5Course.Controllers
 {
-   public class ARController : BaseController
-    {
-        // GET: AR
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+   public class ARController: BaseController
+   {
+      // GET: AR
+      public ActionResult Index() {
+         return View();
+      }
+
+      public ActionResult Index2() {
+         return PartialView("Index");
+      }
+
+      public ActionResult ContentTest() {
+         return Content("<script>alert('Loading...')</script>", "application/javascript", Encoding.UTF8);
+      }
+
+      public ActionResult FileTest() {
+         return File(Server.MapPath("~/Content/imgres.png"), "image/png");
+      }
+   }
 }
