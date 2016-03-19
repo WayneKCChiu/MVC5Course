f46c590ddb6805c43240008515c5a1e7d0673770
邱冠傑 <kuanchieh1102@hotmail.com>
2016-03-19 15:55:59 +0800

    新增分業

--- MVC5Course/Controllers/ClientsController.cs
+++ MVC5Course/Controllers/ClientsController.cs
@@ -3,126 +3,113 @@ using System.Linq;
 using System.Net;
 using System.Web.Mvc;
 using MVC5Course.Models;
+using PagedList;
 
 namespace MVC5Course.Controllers
 {
-   public class ClientsController : Controller
-    {
-        private FabricsEntities db = new FabricsEntities();
+   public class ClientsController: Controller
+   {
+      private FabricsEntities db = new FabricsEntities();
 
-        // GET: Clients
-        public ActionResult Index()
-        {
-            var client = db.Client.Include(c => c.Occupation);
-            return View(client.ToList().Take(5));
-        }
+      // GET: Clients
+      public ActionResult Index(int pageNo = 1) {
+         var client = db.Client.Include(c => c.Occupation).OrderBy(p => p.ClientId);
 
-        // GET: Clients/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Client client = db.Client.Find(id);
-            if (client == null)
-            {
-                return HttpNotFound();
-            }
-            return View(client);
-        }
+         var data = client.ToPagedList(pageNo, 10);
 
-        // GET: Clients/Create
-        public ActionResult Create()
-        {
-            ViewBag.OccupationId = new SelectList(db.Occupation, "OccupationId", "OccupationName");
-            return View();
-        }
 
-        // POST: Clients/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ClientId,FirstName,MiddleName,LastName,Gender,DateOfBirth,CreditRating,XCode,OccupationId,TelephoneNumber,Street1,Street2,City,ZipCode,Longitude,Latitude,Notes")] Client client)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Client.Add(client);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+         return View(data);
+      }
 
-            ViewBag.OccupationId = new SelectList(db.Occupation, "OccupationId", "OccupationName", client.OccupationId);
-            return View(client);
-        }
+      // GET: Clients/Details/5
+      public ActionResult Details(int? id) {
+         if (id == null) {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         Client client = db.Client.Find(id);
+         if (client == null) {
+            return HttpNotFound();
+         }
+         return View(client);
+      }
 
-        // GET: Clients/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Client client = db.Client.Find(id);
-            if (client == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.OccupationId = new SelectList(db.Occupation, "OccupationId", "OccupationName", client.OccupationId);
-            return View(client);
-        }
+      // GET: Clients/Create
+      public ActionResult Create() {
+         ViewBag.OccupationId = new SelectList(db.Occupation, "OccupationId", "OccupationName");
+         return View();
+      }
 
-        // POST: Clients/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ClientId,FirstName,MiddleName,LastName,Gender,DateOfBirth,CreditRating,XCode,OccupationId,TelephoneNumber,Street1,Street2,City,ZipCode,Longitude,Latitude,Notes")] Client client)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(client).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.OccupationId = new SelectList(db.Occupation, "OccupationId", "OccupationName", client.OccupationId);
-            return View(client);
-        }
-
-        // GET: Clients/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Client client = db.Client.Find(id);
-            if (client == null)
-            {
-                return HttpNotFound();
-            }
-            return View(client);
-        }
-
-        // POST: Clients/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Client client = db.Client.Find(id);
-            db.Client.Remove(client);
+      // POST: Clients/Create
+      // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+      // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public ActionResult Create([Bind(Include = "ClientId,FirstName,MiddleName,LastName,Gender,DateOfBirth,CreditRating,XCode,OccupationId,TelephoneNumber,Street1,Street2,City,ZipCode,Longitude,Latitude,Notes")] Client client) {
+         if (ModelState.IsValid) {
+            db.Client.Add(client);
             db.SaveChanges();
             return RedirectToAction("Index");
-        }
+         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+         ViewBag.OccupationId = new SelectList(db.Occupation, "OccupationId", "OccupationName", client.OccupationId);
+         return View(client);
+      }
+
+      // GET: Clients/Edit/5
+      public ActionResult Edit(int? id) {
+         if (id == null) {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         Client client = db.Client.Find(id);
+         if (client == null) {
+            return HttpNotFound();
+         }
+         ViewBag.OccupationId = new SelectList(db.Occupation, "OccupationId", "OccupationName", client.OccupationId);
+         return View(client);
+      }
+
+      // POST: Clients/Edit/5
+      // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+      // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public ActionResult Edit([Bind(Include = "ClientId,FirstName,MiddleName,LastName,Gender,DateOfBirth,CreditRating,XCode,OccupationId,TelephoneNumber,Street1,Street2,City,ZipCode,Longitude,Latitude,Notes")] Client client) {
+         if (ModelState.IsValid) {
+            db.Entry(client).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+         }
+         ViewBag.OccupationId = new SelectList(db.Occupation, "OccupationId", "OccupationName", client.OccupationId);
+         return View(client);
+      }
+
+      // GET: Clients/Delete/5
+      public ActionResult Delete(int? id) {
+         if (id == null) {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         Client client = db.Client.Find(id);
+         if (client == null) {
+            return HttpNotFound();
+         }
+         return View(client);
+      }
+
+      // POST: Clients/Delete/5
+      [HttpPost, ActionName("Delete")]
+      [ValidateAntiForgeryToken]
+      public ActionResult DeleteConfirmed(int id) {
+         Client client = db.Client.Find(id);
+         db.Client.Remove(client);
+         db.SaveChanges();
+         return RedirectToAction("Index");
+      }
+
+      protected override void Dispose(bool disposing) {
+         if (disposing) {
+            db.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+   }
 }
